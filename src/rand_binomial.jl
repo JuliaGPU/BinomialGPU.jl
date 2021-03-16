9bab55b85b495fafc3e36944cdf0b82f1bd60b76
Simone Carlo Surace <simsurace@gmx.net>
2021-03-16 17:56:37 +0100

    move kernel launchers

--- src/rand_binomial.jl
+++ src/rand_binomial.jl
@@ -22,3 +22,36 @@ rand_binomial(T::Type, dim1::Integer, dims::Integer...; kwargs...) =
 
 rand_binomial(dim1::Integer, dims::Integer...; kwargs...) =
     CURAND.rand_binomial(gpuarrays_rng(), Dims((dim1, dims...)); kwargs...)
+
+
+# constant parameters
+function rand_binomial!(rng, A::DenseCuArray{Int}; count::Integer, prob::Number)
+    begin
+        @cuda threads=256 blocks=ceil(Int, length(A)/256) kernel_binomial_const!(A, Int(count), Float32(prob), rng.state)
+    end
+    return A
+end
+
+# arrays of parameters
+function rand_binomial!(rng, A::DenseCuArray{Int}; count::AbstractArray{<:Integer}, prob::AbstractArray{<:Number})
+    cucount = cu(counts)
+    cuprob  = cu(probs)
+    return rand_binomial!(rng, A, cucount, cuprob)
+end
+
+function rand_binomial!(rng, A::DenseCuArray{Int}; count::DenseCuArray{Int}, prob::DenseCuArray{Float32})
+    if size(A) == size(count) == size(prob)
+        begin
+            @cuda threads=256 blocks=ceil(Int, length(A)/256) kernel_binomial_full!(A, count, prob, rng.state)
+        end
+    elseif ndims(count) > ndims(A) || ndims(prob) > ndims(A)
+        throw(DimensionMismatch("`count` and `prob` need to have smaller number of dimensions than `A`"))
+    elseif ndims(A) == 2
+        nothing
+    elseif ndims(A) == 3
+        nothing
+    elseif ndims(A) > 3
+        nothing
+    end
+    return A
+end
