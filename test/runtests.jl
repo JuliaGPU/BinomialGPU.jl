52e9e29cde58366b081a281f3fcac8c4570cd45f
simsurace <simsurace@gmx.net>
2022-02-15 12:27:13 +0100

    Clean up benchmark code

--- test/runtests.jl
+++ test/runtests.jl
@@ -202,17 +202,16 @@ using Test
     end # Distributional tests
 
     @testset "benchmarks" begin
-        # benchmarks
         A = CUDA.zeros(Int, 1024, 1024)
-        n = 128
-        p = 0.5
-        ns = CUDA.fill(128, (1024, 1024))
-        ps = CUDA.rand(1024, 1024)
         println("")
         println("Benchmarking constant parameter array: should run in less than 2ms on an RTX20xx card")
+        n = 128
+        p = 0.5
         display(@benchmark CUDA.@sync rand_binomial!($A, count = $n, prob = $p))
         println("")
         println("Benchmarking full parameter array: should run in less than 2ms on an RTX20xx card")
+        ns = CUDA.fill(128, 1024, 1024)
+        ps = CUDA.fill(0.5f0, 1024, 1024)
         display(@benchmark CUDA.@sync rand_binomial!($A, count = $ns, prob = $ps))
         println("")
     end
